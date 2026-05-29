5f4bce6f419b56ea3cf4cd984dd079addb4920da
ThanhTam-16 <nguyenthanhtam10062004@gmail.com>
2026-05-29 22:51:14 +0700

    prepare backend deployment with neon and prisma 7

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const { notFound, errorHandler } = require("./middlewares/error.middleware");
 
 const app = express();
 
-const allowedOrigins = [env.frontendUrl, "http://localhost:5173", "http://localhost:5174", "http://localhost:3000"];
+const allowedOrigins = [env.frontendUrl, "http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://tzaita.vercel.app"];
 
 app.use(
   cors({
